72af09ccb51755e87b4cfc0dcbf21f8e16790856
islahexpo <islahexpo@gmail.com>
2023-01-25 20:59:15 +0800

    fitur copy format booking

--- js/script.js
+++ js/script.js
@@ -13,3 +13,15 @@ document.addEventListener("click", function (e) {
     navbarNav.classList.remove("active");
   }
 });
+
+function copyFunction() {
+  const copyText = document.getElementById("myInput").textContent;
+  const textArea = document.createElement("textarea");
+  textArea.textContent = copyText;
+  document.body.append(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  k2button.innerText = "Text copied";
+  textArea.remove();
+}
+document.getElementById("k2button").addEventListener("click", copyFunction);
